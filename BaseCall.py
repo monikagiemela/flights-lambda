22c44a79cc733fbd8fe89c295829c46afea94c96
Monika Giemela <monikagiemela@gmail.com>
2022-07-10 12:18:02 +0200

    user-agent

--- BaseCall.py
+++ BaseCall.py
@@ -13,6 +13,8 @@ class BaseCall:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = '/opt/chrome/chrome'
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.0 Safari/537.36")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--start-fullscreen')
@@ -20,6 +22,7 @@ class BaseCall:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-dev-tools")
         chrome_options.add_argument("--no-zygote")
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_argument(f"--user-data-dir={mkdtemp()}")
         chrome_options.add_argument(f"--data-path={mkdtemp()}")
         chrome_options.add_argument(f"--disk-cache-dir={mkdtemp()}")
