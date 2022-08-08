35e1f639b51900f7c8843047ca56da666385afe0
Monika Giemela <monikagiemela@gmail.com>
2022-08-08 21:40:35 +0200

    Esky

--- MyFlights.py
+++ MyFlights.py
@@ -1,4 +1,6 @@
 #cabin=p,e,bfbe;
+# "https://www.{site}/{self.origin}-{self.destination}/{journey['outbound']}/{journey['returning']}?fs=takeoff={self.takeoff_1}__{self.takeoff_2};stops=0;airports={self.origin},{self.destination}&sort=price_a"
+# https://www.esky.pl/flights/select/roundtrip/ap/waw/mp/rom?departureDate=2022-08-13&filter%5Bstops%5D=0&pa=2&page=1&pc=0&pi=0&py=0&returnDate=2022-08-16&sc=economy
 from os import path
 import time
 import csv
@@ -12,12 +14,12 @@ class MyFlights(BaseCall):
         super().__init__()
         self.takeoff_1 = "0700,1000"
         self.takeoff_2 = "1700,2100"
-        self.origin = "WAW"
-        self.destination = "ROM"
+        self.origin = "waw"
+        self.destination = "rom"
         self.sites = ["kayak.com/flights", "momondo.com/flight-search"]
         
-    def make_url(self, journey, site):
-        url = f"https://www.{site}/{self.origin}-{self.destination}/{journey['outbound']}/{journey['returning']}?fs=takeoff={self.takeoff_1}__{self.takeoff_2};stops=0;airports={self.origin},{self.destination}&sort=price_a" 
+    def make_url(self, journey):
+        url = f"https://www.esky.pl/flights/select/roundtrip/ap/{self.origin}/mp/{self.destination}?departureDate={journey['outbound']}&filter%5Bstops%5D=0&pa=2&page=1&pc=0&pi=0&py=0&returnDate={journey['returning']}&sc=economy"
         return url
 
     def save_to_file(self, prices, journey, file):    
@@ -34,13 +36,13 @@ class MyFlights(BaseCall):
     def execute(self):
         self.make_journeys(14, 5)
         self.make_journeys(30, 5)
-        for site in self.sites:
-            with open("/tmp/flight_prices.csv", "w", newline='') as file:
-                for journey in self.journeys:
-                    url = self.make_url(journey, site)
-                    prices = self.send_request(url)
-                    time.sleep(5)
-                    self.save_to_file(prices, journey, file)
-                    time.sleep(5)
-            send_email(self.origin, self.destination, url)
-            print(f"Sent email with {self.origin}-{self.destination} for dates {journey['outbound']}-{journey['returning']}")
+        #for site in self.sites:
+        with open("/tmp/flight_prices.csv", "w", newline='') as file:
+            for journey in self.journeys:
+                url = self.make_url(journey)
+                prices = self.send_request(url)
+                time.sleep(5)
+                self.save_to_file(prices, journey, file)
+                time.sleep(5)
+        send_email(self.origin, self.destination, url)
+        print(f"Sent email with {self.origin}-{self.destination} for dates {journey['outbound']}-{journey['returning']}")
